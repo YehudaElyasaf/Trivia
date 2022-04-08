ffeb5d4668ce7b76109cc11ffe50ee0ddaf139aa
g33king <turhuhxckuo@gmail.com>
2022-04-08 15:32:06 +0300

    Add header files for this version

---
+++ Trivia/Communicator.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <WinSock2.h>
+#include <Windows.h>
+#include <map>
+#include "IRequestHandler.h"
+
+class Communicator
+{
+public:
+	void startHandleRequests();
+
+private:
+	SOCKET _serverSocket;
+	std::map<SOCKET, IRequestHandler*> _clients;
+};
+
