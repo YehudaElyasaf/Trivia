25da28a5193ea47d0f50cf220c65f808dd0cafdd
Yehuda Elyasaf <hvusvvnkl12345@gmail.com>
2022-04-11 23:40:18 +0300

    wrote basic client demo

--- clientDemo/clientDemo.py
+++ clientDemo/clientDemo.py
@@ -1,1 +1,14 @@
+import socket
 
+PORT = 4242
+IP = '127.0.0.1'
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    try:
+        sock.connect((IP, PORT))
+        sock.send('Hello'.encode());
+
+    except socket.error as e:
+        print('Error!')
+        print(e)
