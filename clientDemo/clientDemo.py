ddfffef046b848c1f8342306b5881d1f8129bf67
yeda <30yehuda26@gmail.com>
2022-04-13 16:59:54 +0300

    changed client to sebd messages infinitily

--- clientDemo/clientDemo.py
+++ clientDemo/clientDemo.py
@@ -3,11 +3,16 @@ import socket
 PORT = 4242
 IP = '127.0.0.1'
 
+BUFFER_SIZE = 1024
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     try:
         sock.connect((IP, PORT))
-        sock.send('Hello'.encode());
+        print(sock.recv(BUFFER_SIZE).decode())
+
+        while True:
+            command = input('Enter message to server: ')
+            sock.send(command.encode());
 
     except socket.error as e:
         print('Error!')
