c69cf8d4f696ef91e510c43a6ddf198a9bb100af
yeda <30yehuda26@gmail.com>
2022-05-01 14:51:11 +0300

    wrote docstring to isRequestRelevant()

--- Trivia/LoginRequestHandler.h
+++ Trivia/LoginRequestHandler.h
@@ -4,6 +4,7 @@
 
 class LoginRequestHandler : public IRequestHandler {
 public:
+	//check if the message code is valid
 	bool isRequestRelevant(RequestInfo req) override;
 	RequestResult handleRequest(RequestInfo req) override;
 };
