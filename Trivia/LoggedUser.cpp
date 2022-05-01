30d6db35b814e59dfb1d30f0a98e55551d20b0f9
yeda <30yehuda26@gmail.com>
2022-05-01 21:11:08 +0300

    wrote struct LoggedUser

--- Trivia/LoggedUser.cpp
+++ Trivia/LoggedUser.cpp
@@ -1,2 +1,9 @@
 #include"LoggedUser.h"
- 
+
+LoggedUser::LoggedUser(const std::string& name) {
+	m_username = name;
+}
+
+std::string LoggedUser::getUserName() const {
+	return m_username;
+}
--- Trivia/LoggedUser.h
+++ Trivia/LoggedUser.h
@@ -1,8 +1,9 @@
 #include<vector>
 #include<string>
 
-class LoggedUser {
+struct LoggedUser {
 public:
+	LoggedUser(const std::string& name);
 	std::string getUserName() const;
 private:
 	std::string m_username;
