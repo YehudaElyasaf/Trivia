7d41fd5f1354a06b801dd3f0306d5560969fe8a1
yeda <30yehuda26@gmail.com>
2022-05-01 20:36:33 +0300

    wrote LoginManager::c'tor and d'tor

--- Trivia/LoginManager.cpp
+++ Trivia/LoginManager.cpp
@@ -1,1 +1,22 @@
 #include "LoginManager.h"
+#include"SQLiteDatabase.h"
+
+LoginManager::LoginManager() {
+	m_database = new SQLiteDatabase;
+}
+
+LoginManager::~LoginManager() {
+	delete m_database;
+}
+
+void LoginManager::signup(const std::string& name, const std::string& password, const std::string& mail) {
+
+}
+
+void LoginManager::login(const std::string& name, const std::string& password) {
+
+}
+
+void LoginManager::logout(const std::string& name) {
+
+}
