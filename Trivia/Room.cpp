96c307f21eb33b634717f56aaf5f5520571c11f7
yeda <30yehuda26@gmail.com>
2022-05-07 22:59:18 +0300

    wrote Room::removeUser

--- Trivia/Room.cpp
+++ Trivia/Room.cpp
@@ -1,22 +1,28 @@
 #include"Room.h"
 
-Room::Room(const LoggedUser& firstUser, const RoomData& roomData){
-    m_users.push_back(firstUser);
-    m_metadata = roomData;
+Room::Room(const LoggedUser& firstUser, const RoomData& roomData) {
+	m_users.push_back(firstUser);
+	m_metadata = roomData;
 }
 
-bool Room::addUser(const LoggedUser& userToAdd){
-    for (LoggedUser user : m_users)
-        if (user == userToAdd)
-            return false;
+bool Room::addUser(const LoggedUser& userToAdd) {
+	for (LoggedUser user : m_users)
+		if (user == userToAdd)
+			return false;
 
-    m_users.push_back(userToAdd);
-    return true;
+	m_users.push_back(userToAdd);
+	return true;
 }
-bool Room::removeUser(const LoggedUser& user){
+bool Room::removeUser(const LoggedUser& userToRemove) {
+	for (int i = 0; i < m_users.size(); i++)
+		if (m_users[i] == userToRemove) {
+			m_users.erase(m_users.begin() + i);
+			return true;
+		}
 
+	return false;
 }
 
-std::vector<LoggedUser> Room::getAllUsers() const{
-    return m_users;
+std::vector<LoggedUser> Room::getAllUsers() const {
+	return m_users;
 }
