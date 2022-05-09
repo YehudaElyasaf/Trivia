ec3c38310afa49d26afffece2f4ebcd3ce5aaafd
yeda <30yehuda26@gmail.com>
2022-05-09 17:50:51 +0300

    wrote StatisticsManager::StatisticsManager()

---
+++ Trivia/StatisticsManager.cpp
@@ -0,0 +1,10 @@
+#include"StatisticsManager"
+
+StatisticsManager::StatisticsManager(const IDatabase* db)
+ : m_database(db) {}
+std::vector<std::string> StatisticsManager::getHighScore(){
+
+}
+  std::vector<std::string> StatisticsManager::getUserStatistics(const std::string& username){
+
+}
---
+++ Trivia/StatisticsManager.h
@@ -0,0 +1,14 @@
+#pragma once
+#include"IDatabase.h"
+#include<vector>
+#include<string>
+
+class StatisticsManager
+{
+public:
+    StatisticsManager(const IDatabase* db);
+    std::vector<std::string> getHighScore();
+    std::vector<std::string> getUserStatistics(const std::string& username);
+private:
+    IDatabase* m_database;
+};
