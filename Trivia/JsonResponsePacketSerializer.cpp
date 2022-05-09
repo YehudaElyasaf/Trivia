4be7f2b73ac631da58a104343e71a3ac185c289a
g33king <turhuhxckuo@gmail.com>
2022-05-09 17:59:59 +0300

    Finish all the serializeResponse functions

--- Trivia/JsonResponsePacketSerializer.cpp
+++ Trivia/JsonResponsePacketSerializer.cpp
@@ -12,6 +12,14 @@ std::string JsonResponsePacketSerializer::bitwiseLen(const json& data) {
     return out;
 }
 
+std::string JsonResponsePacketSerializer::join(const std::vector<std::string>& list, const std::string& divider) {
+    std::string out;
+    out = list[0];
+    for (int i = 1; i < list.size(); i++)
+        out += divider + list[i];
+    return out;
+}
+
 std::string JsonResponsePacketSerializer::serializeResponse(const ErrorResponse& resp) {
     std::string out = { (unsigned char)ERROR_CODE };
     json data;
@@ -37,6 +45,81 @@ std::string JsonResponsePacketSerializer::serializeResponse(const SignupResponse
     std::string out = { (unsigned char)SIGNUP_CODE };
     json data;
     data["status"] = resp.status;
+
+    out += bitwiseLen(data);
+    out += data.dump();
+    return out;
+}
+
+std::string JsonResponsePacketSerializer::serializeResponse(const LogoutResponse& resp) {
+    std::string out = { (unsigned char) LOGOUT_CODE };
+    json data;
+    data["status"] = resp.status;
+
+    out += bitwiseLen(data);
+    out += data.dump();
+    return out;
+}
+
+std::string JsonResponsePacketSerializer::serializeResponse(const GetRoomsResponse& resp) {
+    std::string out = {(unsigned char) GET_ROOMS_CODE };
+    json data;
+    data["status"] = resp.status;
+    // cant use join method because it's not vector of strings
+    data["Rooms"] = resp.rooms[0].name;
+    for (int i = 1; i < resp.rooms.size(); i++)
+        data["Rooms"] += DIVIDER + resp.rooms[i].name;
+
+    out += bitwiseLen(data);
+    out += data.dump();
+    return out;
+}
+
+std::string JsonResponsePacketSerializer::serializeResponse(const GetPlayersInRoomResponse& resp) {
+    std::string out = { (unsigned char) GET_PLAYERS_CODE };
+    json data;
+    data["PlayersInRoom"] = join(resp.players);
+    out += bitwiseLen(data);
+    out += data.dump();
+    return out;
+}
+
+std::string JsonResponsePacketSerializer::serializeResponse(const JoinRoomResponse& resp) {
+    std::string out = { (unsigned char) JOIN_ROOM_CODE };
+    json data;
+    data["status"] = resp.status;
+
+    out += bitwiseLen(data);
+    out += data.dump();
+    return out;
+}
+
+std::string JsonResponsePacketSerializer::serializeResponse(const CreateRoomResponse& resp) {
+    std::string out = { (unsigned char) CREATE_ROOM_CODE };
+    json data;
+    data["status"] = resp.status;
+
+    out += bitwiseLen(data);
+    out += data.dump();
+    return out;
+}
+
+std::string JsonResponsePacketSerializer::serializeResponse(const GetHighScoreResponse& resp) {
+    std::string out = { (unsigned char) HIGH_SCORE_CODE };
+    json data;
+    data["status"] = resp.status;
+    data["HighScores"] = join(resp.statistics);
+
+    out += bitwiseLen(data);
+    out += data.dump();
+    return out;
+}
+
+std::string JsonResponsePacketSerializer::serializeResponse(const GetPersonalStatsResponse& resp) {
+    std::string out = { (unsigned char) PERSONAL_STATS_CODE };
+    json data;
+    data["status"] = resp.status;
+    data["UserStatistics"] = join(resp.statistics);
 
     out += bitwiseLen(data);
     out += data.dump();
--- Trivia/JsonResponsePacketSerializer.h
+++ Trivia/JsonResponsePacketSerializer.h
@@ -2,6 +2,8 @@
 #include "responses.h"
 #include "json.h"
 
+#define DIVIDER ", "
+
 using json = nlohmann::json;
 
 class JsonResponsePacketSerializer {
@@ -9,7 +11,16 @@ public:
 	static std::string serializeResponse(const ErrorResponse& resp);
 	static std::string serializeResponse(const LoginResponse& resp);
 	static std::string serializeResponse(const SignupResponse& resp);
+	static std::string serializeResponse(const LogoutResponse& resp);
+	static std::string serializeResponse(const GetRoomsResponse& resp);
+	static std::string serializeResponse(const GetPlayersInRoomResponse& resp);
+	static std::string serializeResponse(const JoinRoomResponse& resp);
+	static std::string serializeResponse(const CreateRoomResponse& resp);
+	static std::string serializeResponse(const GetHighScoreResponse& resp);
+	static std::string serializeResponse(const GetPersonalStatsResponse& resp);
+
 
 	// get the length (in bytes) of a json's string
 	static std::string bitwiseLen(const json& data);
+	static std::string join(const std::vector<std::string>& list, const std::string& divider=DIVIDER);
 };
