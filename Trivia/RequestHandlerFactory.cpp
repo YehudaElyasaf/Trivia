5247a80197286d251ead8c3cd96146771918e608
g33king <turhuhxckuo@gmail.com>
2022-05-09 18:57:31 +0300

    Add methods to factory

--- Trivia/RequestHandlerFactory.cpp
+++ Trivia/RequestHandlerFactory.cpp
@@ -8,6 +8,18 @@ LoginRequestHandler* RequestHandlerFactory::createLoginRequestHandler() {
     return new LoginRequestHandler(m_loginManager, *this);
 }
 
+MenuRequestHandler* RequestHandlerFactory::createMenuRequestHandler() {
+    return new MenuRequestHandler();
+}
+
 LoginManager& RequestHandlerFactory::getLoginManager() {
     return m_loginManager;
+}
+
+StatisticsManager& RequestHandlerFactory::getStatisticsManager() {
+    return m_statisticsManager;
+}
+
+RoomManager& RequestHandlerFactory::getRoomManager() {
+    return m_roomManager;
 }
--- Trivia/RequestHandlerFactory.h
+++ Trivia/RequestHandlerFactory.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "IDatabase.h"
 #include "LoginRequestHandler.h"
+#include "MenuRequestHandler.h"
 #include "LoginManager.h"
 
 class LoginRequestHandler;
@@ -8,11 +9,18 @@ class LoginRequestHandler;
 class RequestHandlerFactory {
 public:
 	RequestHandlerFactory(IDatabase* database);
+	
 	LoginRequestHandler* createLoginRequestHandler();
+	MenuRequestHandler* createMenuRequestHandler();
+	
 	LoginManager& getLoginManager();
+	StatisticsManager& getStatisticsManager();
+	RoomManager& getRoomManager();
 
 private:
 	LoginManager m_loginManager;
+	RoomManager m_roomManager;
+	StatisticsManager m_statisticsManager;
 	IDatabase* m_database;
 };
 
