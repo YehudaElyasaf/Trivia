81c2ffd06691ee76c857eed26e15a75d0d83e862
g33king <turhuhxckuo@gmail.com>
2022-05-19 03:30:11 -0700

    Write Message.ToString and Message.intToBytes

--- GuiClient/Message.cs
+++ GuiClient/Message.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,19 +14,34 @@ namespace GuiClient
             data = _data;
         }
 
+        // deserialization
         public Message(String buffer)
         {
             // Add deserialization here
         }
 
+        // serialization
         override public String ToString()
         {
-            // Add serialization here
-            return "";
+            String buffer = "";
+            buffer += (char)((byte)this.code);
+            String jsonData = JsonConvert.SerializeObject(this.data);
+            buffer += intToBytes(jsonData.Length);
+            buffer += jsonData;
+            return buffer;
         }
 
         private int code { get; }
         private Dictionary<String, String> data { get; }
 
+        // return number as a string of bytes.
+        private String intToBytes(int num) {
+            String output = "";
+            byte[] lenBytes = BitConverter.GetBytes(num);
+            for (int i = 0; i < lenBytes.Length; i++) {
+                output += ((char)lenBytes[i]);
+            }
+            return output;
+        }
     }
 }
