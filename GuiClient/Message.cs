e776012973ae2bcff86695c6e30cd874cf17fce3
g33king <turhuhxckuo@gmail.com>
2022-05-19 02:59:57 -0700

    Add Message class (func declarations) and included json module

---
+++ GuiClient/Message.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuiClient
+{
+    internal class Message
+    {
+        public int _code;
+        public Dictionary<String, String> _data;
+        
+        public Message(int code, Dictionary<String, String> data) {
+            _code = code;
+            _data = data;
+        }
+
+        public Message(String buffer)
+        {
+            // Add deserialization here
+        }
+
+        override public String ToString()
+        {
+            // Add serialization here
+            return "";
+        }
+    }
+}
