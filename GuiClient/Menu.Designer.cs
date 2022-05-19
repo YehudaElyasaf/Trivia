7aea53cf2ed84eb16a1df83cdda0dafb2c6033ce
g33king <turhuhxckuo@gmail.com>
2022-05-19 02:06:37 -0700

    Add a menu design

---
+++ GuiClient/Menu.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace GuiClient
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.createRoomButton = new System.Windows.Forms.Button();
+            this.joinRoomButton = new System.Windows.Forms.Button();
+            this.statsButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // createRoomButton
+            // 
+            this.createRoomButton.Location = new System.Drawing.Point(335, 32);
+            this.createRoomButton.Name = "createRoomButton";
+            this.createRoomButton.Size = new System.Drawing.Size(108, 23);
+            this.createRoomButton.TabIndex = 0;
+            this.createRoomButton.Text = "Create a Room";
+            this.createRoomButton.UseVisualStyleBackColor = true;
+            // 
+            // joinRoomButton
+            // 
+            this.joinRoomButton.Location = new System.Drawing.Point(335, 142);
+            this.joinRoomButton.Name = "joinRoomButton";
+            this.joinRoomButton.Size = new System.Drawing.Size(108, 23);
+            this.joinRoomButton.TabIndex = 1;
+            this.joinRoomButton.Text = "Join a Room";
+            this.joinRoomButton.UseVisualStyleBackColor = true;
+            // 
+            // statsButton
+            // 
+            this.statsButton.Location = new System.Drawing.Point(335, 247);
+            this.statsButton.Name = "statsButton";
+            this.statsButton.Size = new System.Drawing.Size(108, 23);
+            this.statsButton.TabIndex = 2;
+            this.statsButton.Text = "Statistics";
+            this.statsButton.UseVisualStyleBackColor = true;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(335, 367);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(108, 23);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.statsButton);
+            this.Controls.Add(this.joinRoomButton);
+            this.Controls.Add(this.createRoomButton);
+            this.Name = "Menu";
+            this.Text = "Menu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button createRoomButton;
+        private System.Windows.Forms.Button joinRoomButton;
+        private System.Windows.Forms.Button statsButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
