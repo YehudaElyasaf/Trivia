93ba38262df0111fbfe8d85c1d5f9a8b2272652d
328193602 <hvusvvnkl12345@gmail.com>
2022-05-22 13:02:15 +0100

    fixed another conflict

--- GuiClient/MainMenu.Designer.cs
+++ GuiClient/MainMenu.Designer.cs
@@ -34,57 +34,47 @@
             this.createRoomButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // createRoomButton
-            // 
-            this.createRoomButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.createRoomButton.Location = new System.Drawing.Point(327, 31);
-            this.createRoomButton.Name = "createRoomButton";
-            this.createRoomButton.Size = new System.Drawing.Size(152, 34);
-            this.createRoomButton.TabIndex = 0;
-            this.createRoomButton.Text = "Create a Room";
-            this.createRoomButton.UseVisualStyleBackColor = true;
-            // 
-            // statsButton
-            // 
-            this.statsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.statsButton.Location = new System.Drawing.Point(313, 185);
-            this.statsButton.Name = "statsButton";
-            this.statsButton.Size = new System.Drawing.Size(152, 34);
-            this.statsButton.TabIndex = 6;
-            this.statsButton.Text = "Statistics";
-            this.statsButton.UseVisualStyleBackColor = true;
-            // 
-            // joinRoomButton
-            // 
-            this.joinRoomButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.joinRoomButton.Location = new System.Drawing.Point(327, 141);
-            this.joinRoomButton.Name = "joinRoomButton";
-            this.joinRoomButton.Size = new System.Drawing.Size(152, 34);
-            this.joinRoomButton.TabIndex = 5;
-            this.joinRoomButton.Text = "Join a Room";
-            this.joinRoomButton.UseVisualStyleBackColor = true;
-            // 
-            // statsButton
-            // 
-            this.statsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.statsButton.Location = new System.Drawing.Point(327, 256);
-            this.statsButton.Name = "statsButton";
-            this.statsButton.Size = new System.Drawing.Size(152, 34);
-            this.statsButton.TabIndex = 2;
-            this.statsButton.Text = "Statistics";
-            this.statsButton.UseVisualStyleBackColor = true;
-            // 
             // exitButton
             // 
             this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.exitButton.Location = new System.Drawing.Point(327, 366);
+            this.exitButton.Location = new System.Drawing.Point(288, 359);
             this.exitButton.Name = "exitButton";
             this.exitButton.Size = new System.Drawing.Size(152, 34);
             this.exitButton.TabIndex = 3;
             this.exitButton.Text = "Exit";
             this.exitButton.UseVisualStyleBackColor = true;
             // 
-            // Menu
+            // statsButton
+            // 
+            this.statsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statsButton.Location = new System.Drawing.Point(288, 249);
+            this.statsButton.Name = "statsButton";
+            this.statsButton.Size = new System.Drawing.Size(152, 34);
+            this.statsButton.TabIndex = 2;
+            this.statsButton.Text = "Statistics";
+            this.statsButton.UseVisualStyleBackColor = true;
+            // 
+            // joinRoomButton
+            // 
+            this.joinRoomButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.joinRoomButton.Location = new System.Drawing.Point(288, 134);
+            this.joinRoomButton.Name = "joinRoomButton";
+            this.joinRoomButton.Size = new System.Drawing.Size(152, 34);
+            this.joinRoomButton.TabIndex = 5;
+            this.joinRoomButton.Text = "Join a Room";
+            this.joinRoomButton.UseVisualStyleBackColor = true;
+            // 
+            // createRoomButton
+            // 
+            this.createRoomButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.createRoomButton.Location = new System.Drawing.Point(288, 24);
+            this.createRoomButton.Name = "createRoomButton";
+            this.createRoomButton.Size = new System.Drawing.Size(152, 34);
+            this.createRoomButton.TabIndex = 0;
+            this.createRoomButton.Text = "Create a Room";
+            this.createRoomButton.UseVisualStyleBackColor = true;
+            // 
+            // MainMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -92,9 +82,9 @@
             this.Controls.Add(this.statsButton);
             this.Controls.Add(this.joinRoomButton);
             this.Controls.Add(this.createRoomButton);
-            this.Name = "Menu";
-            this.Text = "Menu";
-            this.Load += new System.EventHandler(this.Menu_Load);
+            this.Name = "MainMenu";
+            this.Size = new System.Drawing.Size(746, 482);
+            this.Load += new System.EventHandler(this.MainMenu_Load);
             this.ResumeLayout(false);
 
         }
