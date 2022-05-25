479824bed5c5b7a1d30453d7d1e68d7ec62921ab
328193602 <hvusvvnkl12345@gmail.com>
2022-05-25 11:50:42 +0100

    designed statistics screen

---
+++ GuiClient/Statistics.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuiClient
+{
+    public partial class Statistics : UserControl
+    {
+        public Statistics()
+        {
+            InitializeComponent();
+        }
+
+        private void Statistics_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
