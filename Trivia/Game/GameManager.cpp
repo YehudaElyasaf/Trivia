8f94e841fa101d86bdc2735c580009e8cb187cdc
328193602 <hvusvvnkl12345@gmail.com>
2022-06-07 16:26:40 +0100

    wrote GameManager::createGame

--- Trivia/Game/GameManager.cpp
+++ Trivia/Game/GameManager.cpp
@@ -2,3 +2,11 @@
 
 GameManager::GameManager(const IDatabase* database) :
 	m_database(database) {}
+
+Game GameManager::createGame(const Room& room)
+{
+	std::vector<Question> questions = m_database->getQuestions(room.getRoomData().numOfQuestionsInGame);
+
+	Game game(questions, room.getAllUsers());
+	m_games.push_back(game);
+}
