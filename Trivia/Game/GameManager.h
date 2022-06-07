4a4cbdeb0c8782ac3c2df3e16ecbe5122cf7db70
328193602 <hvusvvnkl12345@gmail.com>
2022-06-07 15:46:03 +0100

    defined class GameManager

---
+++ Trivia/Game/GameManager.h
@@ -0,0 +1,25 @@
+#pragma once
+#include"../Login/LoggedUser.h"
+#include"../Game/Question.h"
+#include<vector>
+#include<map>
+#include<string>
+
+struct GamaData {
+	Question currentQuestion;
+	unsigned int correctAnswerCount;
+	unsigned int wrongAnswerCount;
+	unsigned int averageAnswerTime;
+};
+
+class GameManager {
+private:
+	std::vector<Question> m_questions;
+	std::map<LoggedUser, GamaData> m_players;
+
+public:
+	GameManager(const std::map<LoggedUser, GamaData>& players);
+	Question getQuestionForUser(const LoggedUser& user);
+	void submitAnswer(const LoggedUser& user, const std::string& answer);
+	void removePlayer(const LoggedUser& user);
+};
