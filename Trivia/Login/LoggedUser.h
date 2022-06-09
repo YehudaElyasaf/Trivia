4e94f11cd2b739c762611e663f423fe7e0dede62
328193602 <hvusvvnkl12345@gmail.com>
2022-06-09 20:40:22 +0300

    wrote loggeduser's operator<(const LoggedUser& l, const LoggedUser& r);

--- Trivia/Login/LoggedUser.h
+++ Trivia/Login/LoggedUser.h
@@ -6,4 +6,5 @@ struct LoggedUser {
 	std::string m_username;
 	LoggedUser(const std::string& name);
 	friend bool operator==(const LoggedUser& user1, const LoggedUser& user2);
+	friend bool operator<(const LoggedUser& l, const LoggedUser& r);
 };
